214c1ce5173ecb718194628b8c286f204fde7a0b
Pipexlul <felipgg@gmail.com>
2023-05-08 21:25:03 -0400

    Start adding routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: null,
+    errorElement: null,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" />,
+      },
+      {
+        path: "/home",
+        element: <h1>Home</h1>,
+      },
+      {
+        path: "/cliente",
+        element: <h1>Cliente</h1>,
+      },
+      {
+        path: "/propietario",
+        element: <h1>Propietario</h1>,
+      },
+      {
+        path: "/registro",
+        element: <h1>Registro</h1>,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
