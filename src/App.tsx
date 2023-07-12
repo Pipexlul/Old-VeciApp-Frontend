62a2298f4e5aceb95ca8cbb0b3615a01530b275a
Pipexlul <felipgg@gmail.com>
2023-07-11 23:48:11 -0400

    Wrap main App component in primereact provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from "react-router-dom";
+import { PrimeReactProvider } from "primereact/api";
 
 import routerConfig from "./routes";
 
-const App = () => <RouterProvider router={routerConfig} />;
+const App = () => {
+  return (
+    <PrimeReactProvider>
+      <RouterProvider router={routerConfig} />;
+    </PrimeReactProvider>
+  );
+};
 
 export default App;
