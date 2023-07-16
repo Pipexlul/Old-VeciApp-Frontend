14812f1225dadcbd4ce473d9e3e92869179c9449
Pipexlul <felipgg@gmail.com>
2023-07-15 22:08:07 -0400

    chore: install last dependencies for applying linting and git hooks
    Packages such as lint-staged, prettier, and multiple configs/plugins
    for related dependencies were installed.
    Created necessary config files for prettier and lint-staged.
    Updated eslint config file.
    
    Everything mentioned so far gets applied during a pre-commit hook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,45 @@
-/* eslint-env node */
-
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "react"],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    "react/react-in-jsx-scope": 0,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "import/prefer-default-export": 0,
   },
 };
