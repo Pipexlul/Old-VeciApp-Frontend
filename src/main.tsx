aef7d160199782eebb71e143a452bdc89d966d3f
Pipexlul <felipgg@gmail.com>
2023-07-17 20:38:12 -0400

    style: enable globalStyles in MantineProvider

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider theme={mantineTheme}>
+    <MantineProvider withGlobalStyles theme={mantineTheme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
