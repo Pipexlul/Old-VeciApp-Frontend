7e203944ea1ad23e4da56d0d63f46a769c39054b
Pipexlul <felipgg@gmail.com>
2023-07-31 03:07:01 -0400

    feat(user display): create component to show own user data

---
+++ src/components/ui/UserDisplay.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import {
+  UnstyledButton,
+  UnstyledButtonProps,
+  Group,
+  Avatar,
+  Text,
+  createStyles,
+  Box,
+} from "@mantine/core";
+
+import type { Nullable } from "../../types/UtilityTypes";
+
+import { getFirstLetters } from "../../utils/stringUtils";
+
+const useStyles = createStyles((theme) => ({
+  user: {
+    display: "block",
+    width: "100%",
+    padding: theme.spacing.md,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[8]
+          : theme.colors.gray[0],
+    },
+  },
+}));
+
+interface UserDisplayProps extends UnstyledButtonProps {
+  image: Nullable<string>;
+  name: string;
+  email: string;
+}
+
+const UserDisplay: React.FC<UserDisplayProps> = ({
+  image,
+  name,
+  email,
+  ...others
+}) => {
+  const { classes } = useStyles();
+
+  const fallbackLetters = getFirstLetters(name);
+
+  return (
+    <UnstyledButton className={classes.user} {...others}>
+      <Group>
+        <Avatar src={image} radius="xl" color="blue">
+          {fallbackLetters}
+        </Avatar>
+
+        <Box>
+          <Text size="sm" weight={500}>
+            {name}
+          </Text>
+          <Text color="dimmed" size="xs" truncate>
+            {email}
+          </Text>
+        </Box>
+      </Group>
+    </UnstyledButton>
+  );
+};
+
+export type { UserDisplayProps };
+export default UserDisplay;
